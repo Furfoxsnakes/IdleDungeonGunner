f90b8308ad013e3fb9ff22020a907566e1fc3750
David <dave.pond@gmail.com>
2022-10-18 17:38:34 -0400

    Weapons, weapon firing, ammo, pool manager

---
+++ Assets/Scripts/Arsenal/Ammo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using Utilities;
+
+namespace DefaultNamespace.Arsenal
+{
+    public class Ammo : MonoBehaviour
+    {
+        private Rigidbody2D _rBody;
+        private Vector3 _fireAngle;
+        private AmmoDetailsSO _ammoDetails;
+        private float _range;
+
+        private void Awake()
+        {
+            _rBody = GetComponent<Rigidbody2D>();
+        }
+
+        public void Init(AmmoDetailsSO ammoDetails)
+        {
+            // transform.eulerAngles = lookAt;
+            gameObject.SetActive(true);
+            _ammoDetails = ammoDetails;
+            _fireAngle = HelperUtilities.GetDirectionVectorFromAngle(transform.eulerAngles.z);
+            _range = ammoDetails.Range;
+        }
+
+        private void FixedUpdate()
+        {
+            var distance = _fireAngle * _ammoDetails.Speed * Time.deltaTime;
+            transform.position += distance;
+
+            _range -= distance.magnitude;
+            if (_range <= float.Epsilon)
+                Disable();
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            Disable();
+        }
+
+        private void Disable() => gameObject.SetActive(false);
+
+    }
+}
